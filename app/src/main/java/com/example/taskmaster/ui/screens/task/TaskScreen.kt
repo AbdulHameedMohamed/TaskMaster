2b66f57d9633fee209b41923c4819005aa0c7495
AbdulHameed Mohamed <59577985+AbdulHameedMohamed@users.noreply.github.com>
2024-04-06 21:05:34 +0200

    Fix Bug In Back Pressed From Task Screen

--- app/src/main/java/com/example/taskmaster/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/taskmaster/ui/screens/task/TaskScreen.kt
@@ -1,10 +1,17 @@
 package com.example.taskmaster.ui.screens.task
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
 import com.example.taskmaster.model.data.Priority
 import com.example.taskmaster.model.data.Task
@@ -12,6 +19,7 @@ import com.example.taskmaster.ui.viewmodels.SharedViewModel
 import com.example.taskmaster.util.Action
 
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun TaskScreen(
     selectedTask: Task?,
@@ -23,6 +31,8 @@ fun TaskScreen(
     val priority: Priority by sharedViewModel.priority
 
     val context = LocalContext.current
+
+    BackHandler(onBackPressed = { navigateToListScreen(Action.NO_ACTION) })
 
     Scaffold(
         topBar = {
@@ -66,4 +76,29 @@ fun displayToast(context: Context) {
         "Fields Empty.",
         Toast.LENGTH_SHORT
     ).show()
+}
+
+@Composable
+fun BackHandler(
+    backDispatcher: OnBackPressedDispatcher? =
+        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit
+) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+
+    val backCallback = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+
+    DisposableEffect(key1 = backDispatcher) {
+        backDispatcher?.addCallback(backCallback)
+
+        onDispose {
+            backCallback.remove()
+        }
+    }
 }
